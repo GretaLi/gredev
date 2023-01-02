0520d60cf2c0af2f50ae92e8d8c229212173fa8c
GretaLi <hsinju1993@gmail.com>
2023-01-02 17:41:42 +0800

    Add new project

---
+++ js/data.js
@@ -0,0 +1,216 @@
+export const projectData = [
+  {
+    tab: "website",
+    feature: true,
+    link: "#",
+    repoUrl: "https://github.com/GretaLi/Scopriamo-Roma",
+    liveUrl: "https://gretali.github.io/Scopriamo-Roma/pages/index.html",
+    imgUrl: "./image/roma-travel-multipage-website.png",
+    title: "Roma Travel Website",
+    description:
+      "A multipage website that users can access useful information and know what's worth seeing at the attractions. Registered users can leave a comment for a specific location, collect their favorite attractions and also see them on their own map.",
+    tag: ["HTML", "CSS/SCSS", "Vanilla JS", "API", "GSAP.js", "Leaflet.js"],
+    period: "Dec 2022",
+  },
+  {
+    tab: "website",
+    feature: true,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Product-Landing-Page/tree/main/Yetta%20Studio",
+    liveUrl: "https://gretali.github.io/Product-Landing-Page/Yetta%20Studio/",
+    imgUrl: "./image/yetta-studio-landing-page.png",
+    title: "Yetta’s Studio Landing Page",
+    description:
+      "A landing page for a beauty salon makes it easier for users to access Yetta’s works, price list, FAQ, and client feedback.",
+    tag: ["HTML", "CSS/SCSS", "jQuery"],
+    period: "Aug 2022",
+  },
+
+  {
+    tab: "website",
+    feature: true,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Space-tourism-website-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Space-tourism-website-main/index.html",
+    imgUrl: "./image/space-travel-website.png",
+    title: "Space Travel Website",
+    description:
+      "A responsive, multi-page site features a navbar, tabs, keyboard navigation, and accessibility, built by an effective design system.",
+    tag: ["HTML", "CSS", "Vanilla JS"],
+    period: "Jun 2022",
+  },
+
+  {
+    tab: "webApp",
+    feature: true,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Todo-app-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Todo-app-main/",
+    imgUrl: "./image/todo-list-web-app.png",
+    title: "Todo List Web APP",
+    description:
+      "A responsive todo-web app features add/mark/delete todo functions, filter different todo statuses and toggle light & dark modes.",
+    tag: ["HTML", "CSS", "Vanilla JS"],
+    period: "Sep 2022",
+  },
+
+  {
+    tab: "website",
+    feature: true,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Cowdfunding-product-page-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Cowdfunding-product-page-main/",
+    imgUrl: "./image/crowfunding-product-page.png",
+    title: "Crowdfunding Product Page",
+    description:
+      "A responsive product site features a navbar, bookmark, crowdfunding data display, a progress bar, and an interactive form to make pledges.",
+    tag: ["HTML", "SCSS", "JavaScript", "jQuery"],
+    period: "Sep 2022",
+  },
+
+  {
+    tab: "website",
+    feature: true,
+    link: "#",
+    repoUrl: "https://github.com/GretaLi/chatTalker",
+    liveUrl: "https://gretali.github.io/chatTalker/",
+    imgUrl: "./image/chattalker-landing-page.png",
+    title: "ChatTalker Langing Page",
+    description:
+      "A responsive product landing page for a chat robot service features an interactive price display and FAQ.",
+    tag: ["HTML", "CSS/SCSS", "jQuery"],
+    period: "Jun 2022",
+  },
+
+  {
+    tab: "website",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Intro-section-with-dropdown-navigation-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Intro-section-with-dropdown-navigation-main/",
+    imgUrl: "./image/intro-section-dropdown-navigation.png",
+    title: "Intro Section with Dropdown Navigation",
+    description:
+      "A intro section that users can view the relevant dropdown menus on desktop and mobile when interacting with the navigation links.",
+    tag: ["HTML", "CSS", "JavaScript", "jQuery"],
+    period: "Jul 2022",
+  },
+
+  {
+    tab: "website",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Social-proof-section-master",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Social-proof-section-master/",
+    imgUrl: "./image/social-proof-section.png",
+    title: "Social Proof Section",
+    description:
+      "A social proof section that users can view the optimal layout for the section depending on their device's screen size.",
+    tag: ["HTML", "CSS/SCSS"],
+    period: "Jul 2022",
+  },
+
+  {
+    tab: "webApp",
+    feature: false,
+    link: "#",
+    repoUrl: "https://github.com/GretaLi/Password-generator",
+    liveUrl: "https://gretali.github.io/Password-generator/",
+    imgUrl: "./image/password-generator-web-app.png",
+    title: "Password Generator Web App",
+    description:
+      "A responsive web app to generate two random passwords when the user clicks the button.",
+    tag: ["HTML", "CSS/SCSS"],
+    period: "Sep 2022",
+  },
+
+  {
+    tab: "webApp",
+    feature: false,
+    link: "#",
+    repoUrl: "https://github.com/GretaLi/Unit-converter",
+    liveUrl: "https://gretali.github.io/Unit-converter/",
+    imgUrl: "./image/unit-converter-web-app.png",
+    title: "Unit Converter Web App",
+    description:
+      "A responsive web app to generate all conversions when users clicks the button.",
+    tag: ["HTML", "CSS/SCSS", "jQuery"],
+    period: "Sep 2022",
+  },
+
+  {
+    tab: "component",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Article-preview-component-master",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Article-preview-component-master/",
+    imgUrl: "./image/ariticle-preview-component.png",
+    title: "Article Preview Component",
+    description:
+      "A responsive component that is able to see the social media share links when they click the share icon.",
+    tag: ["HTML", "CSS", "Vanilla JS"],
+    period: "Jun 2022",
+  },
+
+  {
+    tab: "component",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Order-summary-component-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/Order-summary-component-main/",
+    imgUrl: "./image/order-summary-card-component.png",
+    title: "Order Summary Card Component",
+    description:
+      "A order summary card that users cen see hover states for interactive elements.",
+    tag: ["HTML", "SCSS"],
+    period: "Jun 2022",
+  },
+
+  {
+    tab: "component",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/NFT-preview-card-component-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/NFT-preview-card-component-main/",
+    imgUrl: "./image/nft-preview-card-component.png",
+    title: "NFT Preview Card Component",
+    description:
+      "A NFT preview card that users can see hover states for interactive elements.",
+    tag: ["HTML", "SCSS"],
+    period: "Jun 2022",
+  },
+
+  {
+    tab: "component",
+    feature: false,
+    link: "#",
+    repoUrl:
+      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/interactive-rating-component-main",
+    liveUrl:
+      "https://gretali.github.io/Frontend-Mentor-Challenge/interactive-rating-component-main/",
+    imgUrl: "./image/interactive-rating-component.png",
+    title: "Interactive Rating Component",
+    description: `A rating component that users can 
+      select and submit a number rating
+      then see a "Thank you" card after submitting.`,
+    tag: ["HTML", "CSS/SCSS", "jQuery"],
+    period: "Jun 2022",
+  },
+];
--- js/render.js
+++ js/render.js
@@ -1,202 +1,16 @@
 // project page
 
-const data = [
-  {
-    tab: "website",
-    feature: true,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Product-Landing-Page/tree/main/Yetta%20Studio",
-    liveUrl: "https://gretali.github.io/Product-Landing-Page/Yetta%20Studio/",
-    imgUrl: "./image/yetta-studio-landing-page.png",
-    title: "Yetta’s Studio Landing Page",
-    description:
-      "A landing page for a beauty salon makes it easier for users to access Yetta’s works, price list, FAQ, and client feedback.",
-    tag: ["HTML", "CSS/SCSS", "jQuery"],
-  },
+import { projectData } from "./data.js";
+console.log(projectData);
 
-  {
-    tab: "website",
-    feature: true,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Space-tourism-website-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Space-tourism-website-main/index.html",
-    imgUrl: "./image/space-travel-website.png",
-    title: "Space Travel Website",
-    description:
-      "A responsive, multi-page site features a navbar, tabs, keyboard navigation, and accessibility, built by an effective design system.",
-    tag: ["HTML", "CSS", "Vanilla JS"],
-  },
-
-  {
-    tab: "webApp",
-    feature: true,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Todo-app-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Todo-app-main/",
-    imgUrl: "./image/todo-list-web-app.png",
-    title: "Todo List Web APP",
-    description:
-      "A responsive todo-web app features add/mark/delete todo functions, filter different todo statuses and toggle light & dark modes.",
-    tag: ["HTML", "CSS", "Vanilla JS"],
-  },
-
-  {
-    tab: "website",
-    feature: true,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Cowdfunding-product-page-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Cowdfunding-product-page-main/",
-    imgUrl: "./image/crowfunding-product-page.png",
-    title: "Crowdfunding Product Page",
-    description:
-      "A responsive product site features a navbar, bookmark, crowdfunding data display, a progress bar, and an interactive form to make pledges.",
-    tag: ["HTML", "SCSS", "JavaScript", "jQuery"],
-  },
-
-  {
-    tab: "website",
-    feature: true,
-    link: "#",
-    repoUrl: "https://github.com/GretaLi/chatTalker",
-    liveUrl: "https://gretali.github.io/chatTalker/",
-    imgUrl: "./image/chattalker-landing-page.png",
-    title: "ChatTalker Langing Page",
-    description:
-      "A responsive product landing page for a chat robot service features an interactive price display and FAQ.",
-    tag: ["HTML", "CSS/SCSS", "jQuery"],
-  },
-
-  {
-    tab: "website",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Intro-section-with-dropdown-navigation-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Intro-section-with-dropdown-navigation-main/",
-    imgUrl: "./image/intro-section-dropdown-navigation.png",
-    title: "Intro Section with Dropdown Navigation",
-    description:
-      "A intro section that users can view the relevant dropdown menus on desktop and mobile when interacting with the navigation links.",
-    tag: ["HTML", "CSS", "JavaScript", "jQuery"],
-  },
-
-  {
-    tab: "website",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Social-proof-section-master",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Social-proof-section-master/",
-    imgUrl: "./image/social-proof-section.png",
-    title: "Social Proof Section",
-    description:
-      "A social proof section that users can view the optimal layout for the section depending on their device's screen size.",
-    tag: ["HTML", "CSS/SCSS"],
-  },
-
-  {
-    tab: "webApp",
-    feature: false,
-    link: "#",
-    repoUrl: "https://github.com/GretaLi/Password-generator",
-    liveUrl: "https://gretali.github.io/Password-generator/",
-    imgUrl: "./image/password-generator-web-app.png",
-    title: "Password Generator Web App",
-    description:
-      "A responsive web app to generate two random passwords when the user clicks the button.",
-    tag: ["HTML", "CSS/SCSS"],
-  },
-
-  {
-    tab: "webApp",
-    feature: false,
-    link: "#",
-    repoUrl: "https://github.com/GretaLi/Unit-converter",
-    liveUrl: "https://gretali.github.io/Unit-converter/",
-    imgUrl: "./image/unit-converter-web-app.png",
-    title: "Unit Converter Web App",
-    description:
-      "A responsive web app to generate all conversions when users clicks the button.",
-    tag: ["HTML", "CSS/SCSS", "jQuery"],
-  },
-
-  {
-    tab: "component",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Article-preview-component-master",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Article-preview-component-master/",
-    imgUrl: "./image/ariticle-preview-component.png",
-    title: "Article Preview Component",
-    description:
-      "A responsive component that is able to see the social media share links when they click the share icon.",
-    tag: ["HTML", "CSS", "Vanilla JS"],
-  },
-
-  {
-    tab: "component",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/Order-summary-component-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/Order-summary-component-main/",
-    imgUrl: "./image/order-summary-card-component.png",
-    title: "Order Summary Card Component",
-    description:
-      "A order summary card that users cen see hover states for interactive elements.",
-    tag: ["HTML", "SCSS"],
-  },
-
-  {
-    tab: "component",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/NFT-preview-card-component-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/NFT-preview-card-component-main/",
-    imgUrl: "./image/nft-preview-card-component.png",
-    title: "NFT Preview Card Component",
-    description:
-      "A NFT preview card that users can see hover states for interactive elements.",
-    tag: ["HTML", "SCSS"],
-  },
-
-  {
-    tab: "component",
-    feature: false,
-    link: "#",
-    repoUrl:
-      "https://github.com/GretaLi/Frontend-Mentor-Challenge/tree/main/interactive-rating-component-main",
-    liveUrl:
-      "https://gretali.github.io/Frontend-Mentor-Challenge/interactive-rating-component-main/",
-    imgUrl: "./image/interactive-rating-component.png",
-    title: "Interactive Rating Component",
-    description: `A rating component that users can 
-    select and submit a number rating
-    then see a "Thank you" card after submitting.`,
-    tag: ["HTML", "CSS/SCSS", "jQuery"],
-  },
-];
-
-let newData = data.filter(function (obj) {
+let newData = projectData.filter(function (obj) {
   return obj.feature === true;
 });
 
 const tabs = document.querySelectorAll(".tab__btn");
 const projectWrapepr = document.querySelector("#project-wrapper");
+
+let cardHTML = "";
 
 createCard(newData);
 renderCards();
@@ -215,13 +29,13 @@ tabs.forEach((tab) => {
     let isSlected = tab.classList.contains("active");
 
     if (currentTab === "featured" && isSlected) {
-      newData = data.filter(function (obj) {
+      newData = projectData.filter(function (obj) {
         return obj.feature === true;
       });
     }
 
     if (currentTab !== "featured" && isSlected) {
-      newData = data.filter(function (obj) {
+      newData = projectData.filter(function (obj) {
         return obj.tab === currentTab;
       });
     }
@@ -238,7 +52,7 @@ tabs.forEach((tab) => {
 
 function createCard(objArr) {
   cardHTML = "";
-  for (obj of objArr) {
+  for (let obj of objArr) {
     let tab = obj.tab;
     // let link = obj.link;
     let repoUrl = obj.repoUrl;
@@ -247,14 +61,11 @@ function createCard(objArr) {
     let title = obj.title;
     let description = obj.description;
     let tagArr = obj.tag;
+    let period = obj?.period;
     let featured = isFeatured();
 
     function isFeatured() {
-      if (obj.feature) {
-        return "featured";
-      } else {
-        return "";
-      }
+      return obj.feature ? "featured" : "";
     }
 
     let tagHTML = "";
@@ -278,6 +89,9 @@ function createCard(objArr) {
             <div class="project__technique tag">
               ${tags}
             </div>
+            <p>
+              ${period}
+            </p>
         </div>
         <div class="project__modal-overlay">
           <div class="project__modal">
@@ -297,7 +111,7 @@ function createCard(objArr) {
     `;
 
     function createTagHTML() {
-      for (tag of tagArr) {
+      for (let tag of tagArr) {
         tagHTML += `<span>${tag}</span> `;
       }
       return tagHTML;
